8ccd4788329e2ddab6332ef959189d0311151b4b
prathmesh-halgekar <prats_apr25@yahoo.com>
2017-11-13 22:12:51 +0100

    Adding modules for logging, and separating resources in resource module

--- padre-server/resource.py
+++ padre-server/resource.py
@@ -1,27 +1,15 @@
 from flask_restful import Resource
 from padre.utils import DefaultLogger, ResourceDirectory
 from padre.repository import PadreFileRepository
-import os
-from flask import jsonify
+from flask import jsonify, Response
+from authentication import requires_auth
 from werkzeug.exceptions import HTTPException
 
-from authentication import requires_auth
 
-
-# TODO create a corresponding configuration object. look up best practices
-# data_dir = os.path.expanduser("~/tmp/padre_srv/data")
-# if not os.path.exists(data_dir):
-#     os.makedirs(data_dir)
 repo = PadreFileRepository(ResourceDirectory().create_directory())
-
-
 
 # Register a default logger
 logger = DefaultLogger().get_default_logger()
-# class Resource:
-#
-#     def __init__(self, app):
-#         self.app = app
 
 class DatasetsAPI(Resource):  # Create a RESTful resource for datasets
 
@@ -29,10 +17,7 @@ class DatasetsAPI(Resource):  # Create a RESTful resource for datasets
     def get(self):  # Create GET endpoint
         logger.debug("start")
         logger.debug("end")
-        HTTPException.code = 400
-        raise HTTPException
         return jsonify(data=repo.list())
-        # return "get"
 
     def put(self):  # update a new dataset
         pass
