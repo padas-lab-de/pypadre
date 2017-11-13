fc02ed711a176121a30afab2054524cf8776ebab
prathmesh-halgekar <prats_apr25@yahoo.com>
2017-11-13 17:28:16 +0100

    Added constants file.

---
+++ padre/constants.py
@@ -0,0 +1,10 @@
+"""
+  Module handling all constants / properties value to be used. All such declarations should go here.
+"""
+DEFAULT_APP_LOGPATH = "logs/app.log"
+DEFAULT_FORMAT = "%(asctime)s : %(levelname)s : %(filename)s : %(funcName)s : %(lineno)d : %(message)s"
+
+DATASETS_RESOURCE_MAPPING = "/api/datasets/"
+DATASET_RESOURCE_MAPPING = "/api/dataset/<name>"
+
+RESOURCE_DIRECTORY_PATH = "~/tmp/padre_srv/data"
