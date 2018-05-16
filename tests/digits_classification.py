81a20b0a246741335d908d51d9416810103c9ae2
fellic <fellic01@gw.uni-passau.de>
2018-05-16 15:45:13 +0200

    Initial commit for classification of digits experiment

---
+++ tests/digits_classification.py
@@ -0,0 +1,52 @@
+'''
+This file runs a digits classification based on the dataset
+present in scikit-learn datasets.
+'''
+import pprint
+
+from padre.ds_import import load_sklearn_toys
+from padre.experiment import Experiment
+
+
+def create_test_pipeline():
+    from sklearn.pipeline import Pipeline
+    from sklearn import svm
+    estimators = [('clf', svm.SVC(probability=True, gamma=0.001))]
+    return Pipeline(estimators)
+
+def main():
+
+    # Get the dataset from load_sklearn_toys
+    # The dataset index is 3
+    from padre.app import pypadre
+    pypadre.set_printer(print)
+    # NOTE: Server MUST BE RUNNING!!! See Padre Server!
+    # Start PADRE Server and run
+    ds = None
+    try:
+        pypadre.datasets.list_datasets()
+        ds = pypadre.datasets.get_dataset("http://localhost:8080/api/datasets/5")
+    except:
+        ds = [i for i in load_sklearn_toys()][3]
+
+    print(ds)
+    ex = Experiment(name="Digits Recognition",
+                    description="Testing Support Vector Classification via SKLearn Pipeline",
+                    dataset=ds,
+                    workflow=create_test_pipeline(),
+                    backend=pypadre.file_repository.experiments)
+
+    conf = ex.configuration()  # configuration, which has been automatically extracted from the pipeline
+    pprint.pprint(ex.hyperparameters())  # get and print hyperparameters
+    ex.run()  # run the experiment and report
+    print("========Available experiments=========")
+    for idx, ex in enumerate(pypadre.experiments.list_experiments()):
+        print("%d: %s" % (idx, str(ex)))
+        for idx2, run in enumerate(pypadre.experiments.list_runs(ex)):
+            print("\tRun: %s" % str(run))
+
+    # ex.report_results() # last step, but we can also look that up on the server
+
+
+if __name__ == '__main__':
+    main()
