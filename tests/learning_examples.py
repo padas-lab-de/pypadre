51dea1957af9dd9fd996799ab5c77ab78ce761fc
fellic <fellic01@gw.uni-passau.de>
2018-05-17 12:27:15 +0200

    Added function for creating workflow, experiments, and modifying the parameters of estimators.
    Simulating different missing value scenarios
    
    Code runs multiple experiments serially

--- tests/learning_examples.py
+++ tests/learning_examples.py
@@ -34,16 +34,42 @@ workflow_dict = {'pca': decomposition.PCA(),
                  'SVC': SVC(probability=True),
                  'LSA': decomposition.TruncatedSVD(n_components=2)}
 
+parameters = {'SVC': ['probability'],
+              'LSA': ['n_components']}
 
-def create_test_pipeline(estimator_list):
+
+# This function sets the parameters of the estimators and classifiers created
+# in the pipeline. The param_val_dict contains the name of the parameter and
+# the value to be set for that parameter
+# The parameters are looked up from the parameters dictionary
+def set_parameters(estimator, estimator_name, param_val_dict):
+    if estimator is None:
+        print(estimator_name + ' does not exist in the workflow')
+        return None
+    available_params = parameters.get(estimator_name)
+    for param in param_val_dict:
+        if param not in available_params:
+            print(param + ' is not present for estimator ' + estimator_name)
+        else:
+            estimator.set_params(**{param: param_val_dict.get(param)})
+
+    return estimator
+
+
+def create_test_pipeline(estimator_list, param_value_dict=None):
     from sklearn.pipeline import Pipeline
     estimators = []
     # estimators=[('pca', decomposition.PCA()), ('logistic', linear_model.LogisticRegression())]
-    for estimator in estimator_list:
-        if workflow_dict.get(estimator) is None:
-            print(estimator + ' not present in list')
+    for estimator_name in estimator_list:
+        if workflow_dict.get(estimator_name) is None:
+            print(estimator_name + ' not present in list')
             return None
-        estimators.append((estimator, workflow_dict.get(estimator)))
+        estimator = workflow_dict.get(estimator_name)
+        estimators.append((estimator_name, estimator))
+        if param_value_dict is not None and \
+                param_value_dict.get(estimator_name) is not None:
+            set_parameters(estimator, estimator_name, param_value_dict.get(estimator_name))
+
     return Pipeline(estimators)
 
 
@@ -54,7 +80,9 @@ def create_experiment(name, description,
                       dataset, workflow,
                       backend):
     # Experiment name should be unique
-    if experiments_dict.get(name, None) is None:
+    if experiments_dict.get(name, None) is None and \
+        not(name is None or description is None or dataset is None
+            or workflow is None or backend is None):
         data_dict = dict()
         data_dict['description'] = description
         data_dict['dataset'] = dataset
@@ -63,8 +91,9 @@ def create_experiment(name, description,
         experiments_dict[name] = data_dict
 
     else:
-        print('Experiment name should be unique')
-        print(name + ' already exists in experiments')
+        print('Error creating experiment')
+        if experiments_dict.get(name, None) is not None:
+            print('Experiment name: ', name, ' already present. Experiment name should be unique')
 
 
 def main():
@@ -86,18 +115,56 @@ def main():
     # Create a sample experiment
     workflow = create_test_pipeline(['SVC'])
     create_experiment(name='Experiment1', description='This is the first test experiment',
-                      dataset=ds, workflow=workflow,
+                      dataset=get_local_dataset('Breast_Cancer'), workflow=workflow,
                       backend=pypadre.file_repository.experiments)
 
     # Create another experiment which has a transformation and logistic regression
     workflow = create_test_pipeline(['pca', 'logistic'])
     create_experiment(name='Experiment2', description='This is the second test experiment',
-                      dataset=[i for i in load_sklearn_toys()][3], workflow=workflow,
+                      dataset=get_local_dataset('Iris'), workflow=workflow,
                       backend=pypadre.file_repository.experiments)
-    workflow = create_test_pipeline(['LSA'])
+
+    # Experiment using SVD in the pipeline
+    # setting parameters for estimator 'LSA'
+    params = {'n_components': 5}
+    param_value_dict = {'LSA': params}
+    workflow = create_test_pipeline(['LSA'], param_value_dict)
+    #set_parameters(workflow,['LSA'],{'n_components':5})
     create_experiment(name='Experiment3', description='This is the third test experiment',
                       dataset=get_local_dataset('Diabetes'), workflow=workflow,
                       backend=pypadre.file_repository.experiments)
+
+    # Testing missing parameters when initializing the Experiment class
+
+    # Missing name
+    workflow = create_test_pipeline(['LSA'])
+    create_experiment(name=None, description='This is the third test experiment',
+                      dataset=get_local_dataset('Diabetes'), workflow=workflow,
+                      backend=pypadre.file_repository.experiments)
+
+    # Missing description
+    workflow = create_test_pipeline(['LSA'])
+    create_experiment(name='Error2', description=None,
+                      dataset=get_local_dataset('Diabetes'), workflow=workflow,
+                      backend=pypadre.file_repository.experiments)
+
+    # Missing dataset
+    workflow = create_test_pipeline(['LSA'])
+    create_experiment(name='Error3', description='This experiment is missing the dataset',
+                      dataset=get_local_dataset('MISSING'), workflow=workflow,
+                      backend=pypadre.file_repository.experiments)
+
+    # Missing workflow
+    workflow = create_test_pipeline(['ERROR'])
+    create_experiment(name='Error4 ', description='This experiment is missing a workflow',
+                      dataset=get_local_dataset('Diabetes'), workflow=workflow,
+                      backend=pypadre.file_repository.experiments)
+
+    # Missing backend
+    workflow = create_test_pipeline(['LSA'])
+    create_experiment(name='Error1 ', description='This experiment is missing a workflow',
+                      dataset=get_local_dataset('Diabetes'), workflow=workflow,
+                      backend=None)
 
     # Run all the experiments in the list
     for experiment in experiments_dict:
