0ee14edc5498825553d9a6f97b9399d500f192f4
fellic <fellic01@gw.uni-passau.de>
2018-05-25 09:19:19 +0200

    Added visualizations from confusion matrix for classification

--- tests/proof_of_concept/visualize_results/visualize.py
+++ tests/proof_of_concept/visualize_results/visualize.py
@@ -36,7 +36,7 @@ class VisualizeResults:
 
         for sub_directory in dir_list:
             if os.path.exists(os.path.join(sub_directory, 'results.json')):
-                self._path_list.append(os.path.join(sub_directory,'results.json'))
+                self._path_list.append(os.path.join(sub_directory, 'results.json'))
 
     def get_immediate_subdirectories(self, dir_path):
         return [os.path.join(dir_path, name) for name in os.listdir(dir_path)
@@ -62,13 +62,59 @@ class VisualizeResults:
         This function finds the confusion matrix from the predicted and truth values
         :return: None
         """
-        colors = iter(cm.rainbow(np.linspace(0, 1, len(self._data))))
-        for json_data in self._data:
+        total_error_matrix = np.empty(shape=[len(set(self._data[0]['truth'])), len(self._data)])
+
+        for json_data, row_idx in zip(self._data, range(0, len(self._data))):
             y_true = copy.deepcopy(json_data.get('truth', None))
             y_pred = copy.deepcopy(json_data.get('predicted', None))
-            self._confusion_matrix = metrics.confusion_matrix(y_true=y_true,
-                                                              y_pred=y_pred)
-            print(self._confusion_matrix)
+            confusion_matrix = metrics.confusion_matrix(y_true=y_true,
+                                                        y_pred=y_pred)
+            print(confusion_matrix)
+            # total test vectors for each class
+            total = np.asarray(np.sum(confusion_matrix, axis=0))
+            err_val = []
+            for idx in range(len(np.asarray(confusion_matrix))):
+                err_val.append(np.asarray(confusion_matrix)[idx][idx]/total[idx])
+                total_error_matrix[idx][row_idx] = confusion_matrix[idx][idx]/total[idx]
+
+        # Create a bar chart for each label in it.
+        idx = 0
+        width = 0.25
+
+        # Display a plot with precision grouped by classifier
+        ind = np.arange(len(total_error_matrix[0]))
+        print(ind)
+        rects = []
+        label_names = ('A', 'B', 'C')
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for row in total_error_matrix:
+            print(row)
+            rects.append(ax.bar(ind + width * idx, row, width))
+            idx = idx + 1
+
+        ax.set_ylabel('Precision')
+        ax.set_title('Precision Scores grouped by Label')
+        ax.legend(rects, label_names)
+        plt.show()
+
+        # Display a bar chart with precision grouped by labels
+        fig_labels = plt.figure()
+        ax_labels = fig_labels.add_subplot(111)
+        rects_transposed = []
+        idx = 0
+        total_error_matrix_transposed = np.transpose(total_error_matrix)
+        width = 0.075
+        ind = np.arange(len(total_error_matrix_transposed[0]))
+        for row in total_error_matrix_transposed:
+            rects_transposed.append(ax_labels.bar(ind + width * idx, row, width))
+            idx = idx + 1
+        ax_labels.set_ylabel('Precision')
+        ax_labels.set_title('Precision Scores grouped by classifier')
+        #ax_labels.legend(rects_transposed, label_names)
+        plt.show()
+
+
 
     def get_regression_metrics(self):
         """
@@ -82,7 +128,7 @@ class VisualizeResults:
             y_pred = copy.deepcopy(json_data.get('predicted', None))
 
             if y_true is not None and y_pred is not None:
-                self._mean_squared_error.append( metrics.mean_squared_error(y_true=y_true,
+                self._mean_squared_error.append(metrics.mean_squared_error(y_true=y_true,
                                                                             y_pred=y_pred))
                 color = next(colors)
                 _plt = plt.scatter(y_true, np.asarray(np.absolute(np.subtract(y_true, y_pred))),
@@ -95,9 +141,6 @@ class VisualizeResults:
         plt.xlabel('Actual Value')
         plt.legend(loc=1)
         plt.show()
-
-
-
 
 
 def main():
@@ -118,11 +161,12 @@ def main():
     path_classification = \
         "/home/chris/.pypadre/experiments/Grid_search_experiment_3.ex/53bd15c0-5e86-11e8-b985-080027031794.run" \
         "/results.json"
+    dir_path = '/home/chris/.pypadre/experiments/Grid_search_experiment_3.ex'
     path_list.append(path_classification)
     visualize_classification = VisualizeResults(path_list)
+    visualize_classification.read_directory(dir_path=dir_path)
     visualize_classification.read_data()
     visualize_classification.get_confusion_matrix()
-
 
 
 if __name__ == '__main__':
