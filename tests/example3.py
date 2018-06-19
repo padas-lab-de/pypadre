6b987b72d4c502908a1768864ddb7e256ee20c46
christofer <fellic01@gw.uni-passau.de>
2018-06-19 10:29:19 +0200

    Example using the new features in Experiment creator class

--- tests/example3.py
+++ tests/example3.py
@@ -12,15 +12,15 @@ def main():
     print(experiment_helper.get_dataset_names())
     experiment_param_dict = dict()
     workflow = experiment_helper.create_test_pipeline(['principal component analysis', 'logistic regression'])
-    params_logistic_pca = {'n_components': [4, 5, 6, 7, 10]}
+    params_logistic_pca = {'num_components': [4, 5, 6, 7, 10]}
     params_dict_logistic = {'principal component analysis': params_logistic_pca}
-    experiment_param_dict['Grid_search_experiment_5'] = copy.deepcopy(params_dict_logistic)
+    #experiment_param_dict['Grid_search_experiment_5'] = copy.deepcopy(params_dict_logistic)
+    experiment_helper.set_param_values('Test Experiment PCA Logistic', params_dict_logistic)
     experiment_helper.create_experiment(name='Test Experiment PCA Logistic',
                                         description='Test Experiment with pca and logistic regression',
                                         dataset=experiment_helper.get_local_dataset('Diabetes'),
                                         workflow=workflow,
-                                        backend=pypadre.file_repository.experiments,
-                                        params=params_dict_logistic)
+                                        backend=pypadre.file_repository.experiments)
     #experiment_helper.set_param_values('Test Experiment PCA Logistic', params_dict_logistic)
     experiment_helper.execute_experiments()
 
