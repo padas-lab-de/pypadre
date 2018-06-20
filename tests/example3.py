24c4b5180ce8946831da1b2ac0f72b1410bccae9
christofer <fellic01@gw.uni-passau.de>
2018-06-20 10:06:10 +0200

    Added code to test run experiments on multiple datasets

--- tests/example3.py
+++ tests/example3.py
@@ -10,16 +10,21 @@ from padre.ExperimentCreator import ExperimentCreator
 def main():
     experiment_helper = ExperimentCreator()
     print(experiment_helper.get_dataset_names())
-    workflow = experiment_helper.create_test_pipeline(['principal component analysis', 'logistic regression'])
+    workflow = experiment_helper.create_test_pipeline(['principal component analysis', 'linear regression'])
     params_logistic_pca = {'num_components': [4, 5, 6, 7, 10]}
     params_dict_logistic = {'principal component analysis': params_logistic_pca}
-    experiment_helper.set_param_values('Test Experiment PCA Logistic', params_dict_logistic)
-    experiment_helper.create_experiment(name='Test Experiment PCA Logistic',
+    experiment_helper.set_param_values('Test Experiment PCA Linear', params_dict_logistic)
+    experiment_helper.create_experiment(name='Test Experiment PCA Linear',
                                         description='Test Experiment with pca and logistic regression',
-                                        dataset=experiment_helper.get_local_dataset('Diabetes'),
+                                        dataset='Diabetes',
                                         workflow=workflow,
                                         backend=pypadre.file_repository.experiments)
     experiment_helper.execute_experiments()
+
+    experiment = ['Test Experiment PCA Linear']
+    datasets = ['Diabetes', 'Boston_House_Prices']
+    experiment_datasets = {experiment[0]:datasets}
+    experiment_helper.do_experiments(experiment_datasets)
 
 
 
