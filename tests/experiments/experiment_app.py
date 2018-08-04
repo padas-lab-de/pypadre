974a3201f50f71bd8d0c64b998fd64e526b8f488
Michael Granitzer <michael.granitzer@uni-passau.de>
2018-08-04 15:28:03 +0200

    added basic tests for exepriments and updated documentation

---
+++ tests/experiments/experiment_app.py
@@ -0,0 +1,26 @@
+"""
+This file shows an example on how to use the pypadre app.
+"""
+import unittest
+
+from padre.ds_import import load_sklearn_toys
+from padre.experiment import Experiment
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+
+
+class TestSklearn(unittest.TestCase):
+
+    def test_SVC(self):
+        ds = [i for i in load_sklearn_toys()]
+        ex = Experiment(name="Test Experiment SVM",
+                        description="Testing Support Vector Machines via SKLearn Pipeline\n"
+                                    "- no persisting via a backend\n"
+                                    "- manual data set loading\n"
+                                    "- default parameters",
+                        dataset=ds[2],
+                        workflow=Pipeline([('clf', SVC(probability=True))]))
+        ex.run()
+        assert len(ex.runs) == 1
+        # TODO: we currently do not store splits and we do have no means to evaluate performance measures
+        # from runs where there is NO BACKEND! If there is no backend, we should keep runs and splits in memory
