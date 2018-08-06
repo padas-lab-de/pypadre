f216460ea35d2e458a62d4936afb8c8a6599802b
Michael Granitzer <michael.granitzer@uni-passau.de>
2018-08-06 09:00:14 +0200

    decorated workflows are now available via importing experiments_decorated

---
+++ tests/app/experiments_decorated.py
@@ -0,0 +1,40 @@
+"""
+the module defines two fast experiments under the name test_AppExp and test_AppExp2.
+
+The test can be used by importing thsi module and running pypadre.experiments.run(decorated=ture)
+```
+from tests.app.experiments_decorated import *
+ex = pypadre.experiments.run(decorated=True)
+```
+"""
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from padre import *
+from padre.ds_import import load_sklearn_toys
+
+
+@Workflow(exp_name="test_AppExp",
+          description="Test experiment with decorators")
+def create_test_pipeline():
+    estimators = [('clf', SVC(probability=True))]
+    return Pipeline(estimators)
+
+
+@Workflow(exp_name="test_AppExp2",
+          description="Test2 experiment with decorators")
+def create_test_pipeline():
+    estimators = [('clf', SVC(probability=True, C=0.2))]
+    return Pipeline(estimators)
+
+
+@Dataset(exp_name="test_AppExp")
+def get_dataset():
+    ds = [i for i in load_sklearn_toys()][2]
+    return ds
+
+
+@Dataset(exp_name="test_AppExp2")
+def get_dataset():
+    ds = [i for i in load_sklearn_toys()][3]
+    return ds
+
--- tests/app/test_app_experiments.py
+++ tests/app/test_app_experiments.py
@@ -6,38 +6,9 @@ Note: it is a proof of concept now rather than a test.
 # Note that we want to include all decorator at once using package import
 import unittest
 
-from sklearn.pipeline import Pipeline
-from sklearn.svm import SVC
 from padre import *
 from padre.app import pypadre
-from padre.ds_import import load_sklearn_toys
-
-
-@Workflow(exp_name="test_AppExp",
-          description="Test experiment with decorators")
-def create_test_pipeline():
-    estimators = [('clf', SVC(probability=True))]
-    return Pipeline(estimators)
-
-
-@Workflow(exp_name="test_AppExp2",
-          description="Test2 experiment with decorators")
-def create_test_pipeline():
-    estimators = [('clf', SVC(probability=True, C=0.2))]
-    return Pipeline(estimators)
-
-
-@Dataset(exp_name="test_AppExp")
-def get_dataset():
-    ds = [i for i in load_sklearn_toys()][2]
-    return ds
-
-
-@Dataset(exp_name="test_AppExp2")
-def get_dataset():
-    ds = [i for i in load_sklearn_toys()][3]
-    return ds
-
+from tests.app.experiments_decorated import *
 
 class TestApp(unittest.TestCase):
 
@@ -45,12 +16,20 @@ class TestApp(unittest.TestCase):
         pypadre.experiments.delete_experiments("test.*")
 
     def test_delete(self):
+        """
+        test delete as prerequsite for upcoming tests
+        :return:
+        """
         self._del_test_exps() # every test should begin with that
         ex_list = pypadre.experiments.list_experiments()
         for ex in ex_list:
             self.assertTrue(not ex.startswith("test"))
 
     def test_sklearn_SVC(self):
+        """
+        test basic experiment handling
+        :return:
+        """
         self._del_test_exps()
         ex_list = pypadre.experiments.list_experiments()
         # Do basic experiment
@@ -69,8 +48,22 @@ class TestApp(unittest.TestCase):
         ex_list_n = pypadre.experiments.list_experiments()
         self.assertTrue("test_APPSKLEARN" not in ex_list_n)
 
-
     def test_decorators(self):
+        """
+        test the use of decorators (via importing experiments_decorated)
+        :return:
+        """
         self._del_test_exps()
+        ex_list = pypadre.experiments.list_experiments()
         ex = pypadre.experiments.run(decorated=True)
+        self.assertTrue(ex is not None)
+        # check its storage
+        ex_list_n = pypadre.experiments.list_experiments()
+        self.assertTrue(len(ex_list) + 2 == len(ex_list_n))
+        self.assertTrue("test_AppExp" in ex_list_n)
+        self.assertTrue("test_AppExp2" in ex_list_n)
         # Now test existence at experiments, list experiments etc.
+        pypadre.experiments.delete_experiments("test_AppExp.*")
+        ex_list_n = pypadre.experiments.list_experiments()
+        self.assertTrue("test_AppExp" not in ex_list_n)
+        self.assertTrue("test_AppExp2" not in ex_list_n)
