a1101b3b5c2561717af1ccf8dcfed577cca1f97e
christofer <fellic01@gw.uni-passau.de>
2018-09-14 10:35:43 +0200

    Removed an unnecessary commented line of code which might cause a bug while writing to hyperparameters.json file

--- padre/backend/file.py
+++ padre/backend/file.py
@@ -202,14 +202,13 @@ class ExperimentFileRepository:
 
         with open(os.path.join(dir, "metadata.json"), 'w') as f:
             f.write(self._metadata_serializer.serialise(experiment.metadata))
-        '''
+
         #Commented for pytorch integration
-        
+        '''
         with open(os.path.join(dir, "hyperparameter.json"), 'w') as f:
             params = experiment.hyperparameters()
-            for key in params:
-                # This writes all data present within the params to the JSON file
-                f.write(self._metadata_serializer.serialise(params))
+            # This writes all data present within the params to the JSON file
+            f.write(self._metadata_serializer.serialise(params))
         '''
 
         with open(os.path.join(dir, "workflow.bin"), 'wb') as f:
