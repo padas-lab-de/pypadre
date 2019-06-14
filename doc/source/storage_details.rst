39b7395ae0700ecf2e9d5e28c7994a294257ee6a
afnan <hm.afnan@yahoo.com>
2019-06-14 11:58:12 +0200

    Documentation for upload and download experiment added

--- doc/source/storage_details.rst
+++ doc/source/storage_details.rst
@@ -9,6 +9,26 @@ Local Storage Structure
 Server Synchronisation
 ----------------------
 
+Upload local experiment
+***********************
+Local experiment can be uploaded to the server if its not already uploaded.
+In the app :func:`pypadre.app.padre_app.ExperimentApp.upload_local_experiment` takes experiment name and uploads
+it to the server if its not already uploaded.
+
+.. autofunction:: pypadre.app.padre_app.ExperimentApp.upload_local_experiment
+
+In the following example we create experiment on local, authenticate and then upload it to server
+
+.. literalinclude:: ../../tests/examples/example_upload_local_experiment.py
+
+Download experiment
+*******************
+Experiment from server can be downloaded on local if its not already exists.
+In the app :func:`pypadre.app.padre_app.ExperimentApp.download_remote_experiment` takes experiment id, name or url
+and downloads it to the local if its not already exists.
+
+.. autofunction:: pypadre.app.padre_app.ExperimentApp.download_remote_experiment
+
 
 Versioning
 **********
--- tests/examples/example_upload_local_experiment.py
+++ tests/examples/example_upload_local_experiment.py
@@ -9,8 +9,6 @@ from pypadre.core import Experiment
 def create_test_pipeline():
     from sklearn.pipeline import Pipeline
     from sklearn.svm import SVC
-    from sklearn.decomposition import PCA
-    # estimators = [('reduce_dim', PCA()), ('clf', SVC())]
     estimators = [('SVC', SVC(probability=True))]
     return Pipeline(estimators)
 
@@ -18,13 +16,11 @@ def create_test_pipeline():
 def create_preprocessing_pipeline():
     from sklearn.pipeline import Pipeline
     from sklearn.decomposition import PCA
-    # estimators = [('reduce_dim', PCA()), ('clf', SVC())]
     estimators = [('PCA', PCA())]
     return Pipeline(estimators)
 
 
 def split(idx):
-    # Do a 70:30 split
     limit = int(.7 * len(idx))
     return idx[0:limit], idx[limit:], None
 
@@ -32,18 +28,7 @@ def split(idx):
 if __name__ == '__main__':
     from pypadre.app import p_app
     p_app.set_printer(print)
-
-    # NOTE: Server MUST BE RUNNING!!! See Padre Server!
-    # Start PADRE Server and run
-    ds = None
-    try:
-        p_app.datasets.list()
-        ds = p_app.datasets.get_dataset("http://localhost:8080/api/datasets/5")
-    except:
-        ds = [i for i in load_sklearn_toys()][4]
-
-    if ds is None:
-        ds = [i for i in load_sklearn_toys()][4]
+    ds = [i for i in load_sklearn_toys()][4]
     print(ds)
     experiment_name = "Test Experiment SVM upload local"
     ex = Experiment(name=experiment_name,
@@ -51,9 +36,10 @@ if __name__ == '__main__':
                     dataset=ds, preprocessing=create_preprocessing_pipeline(),
                     workflow=create_test_pipeline(), keep_splits=True, strategy="random",
                     function=split)
-    conf = ex.configuration()  # configuration, which has been automatically extracted from the pipeline
-    pprint.pprint(ex.hyperparameters())  # get and print hyperparameters
-    ex.execute()  # run the experiment and report
+    conf = ex.configuration()
+    pprint.pprint(ex.hyperparameters())
+    ex.execute()
 
+    # Authenticate and upload
     p_app.authenticate("hmafnan", "test")
     p_app.experiments.upload_local_experiment(experiment_name)
