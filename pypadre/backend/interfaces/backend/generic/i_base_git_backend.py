23d5a766660ce44693f10bc7c8b6cad94ee34f54
Christofer Fellicious <christofer.fellicious@uni-passau.de>
2019-07-24 08:41:02 +0200

    Added commit functionality to the git interface Removed unnecessary functions

--- pypadre/backend/interfaces/backend/generic/i_base_git_backend.py
+++ pypadre/backend/interfaces/backend/generic/i_base_git_backend.py
@@ -4,6 +4,12 @@ from git import Repo
 
 from pypadre.backend.interfaces.backend.generic.i_base_meta_file_backend import IBaseMetaFileBackend
 
+"""
+For datasets, experiments and projects there would be separate repositories.
+The Dataset, experiment and project classes implement the IBaseGitBackend
+So, the only functionalities required by git are add_file, list_file, delete_file, get_file
+"""
+
 
 class IBaseGitBackend(IBaseMetaFileBackend):
     """ This is the abstract class implementation of a class extending the basic file backend with git functionality """
@@ -11,83 +17,119 @@ class IBaseGitBackend(IBaseMetaFileBackend):
     # Variable holding the repository
     _repo = None
     _origin = None
+    _REMOTE_URL = None
+    _REMOTE_NAME = None
+    _remote = None
+    _DEFAULT_GIT_MSG = 'Added file to git'
 
     # Method to create a repository at the root directory of the object.
     # If bare=True, the function creates a bare bones repository
-    @abstractmethod
-    def create_bare_repo(self, bare=True):
-        self.repo = Repo.init(self.root_dir, bare)
 
-    @abstractmethod
-    def create_remote(self, remote_name='origin', url=''):
+    def _create_repo(self, bare=True):
+        """
+        Creates a local repository
+        :param bare: Creates a bare git repository
+        :return:
+        """
+        self.repo = Repo.init(self._root_dir, bare)
+
+    def _create_remote(self, remote_name='origin', url=''):
+        """
+
+        :param remote_name: Name of the remote repository
+        :param url: URL to the remote repository
+        :return:
+        """
         self.origin = self.repo.create_remote(name=remote_name, url=url)
 
-    @abstractmethod
-    def create_head(self, name):
+    def _create_head(self, name):
+        """
+        Creates a new branch
+        :param name: Name of the new branch
+        :return: Object to the new branch
+        """
         new_branch = self.repo.create_head(name)
         assert (self.repo.active_branch != new_branch)
         return new_branch
 
-    @abstractmethod
-    def create_tag(self, tag_name, ref_branch, message):
+    def _create_tag(self, tag_name, ref_branch, message):
+        """
+        Creates a new tag for the branch
+        :param tag_name: Name for the tag
+        :param ref_branch: Branch where the tag is to be created
+        :param message: Message for the tag
+        :return:
+        """
         tag = self.repo.create_tag(tag_name, ref=ref_branch, message=message)
         tag.commit
 
-    @abstractmethod
-    def create_sub_module(self, sub_repo_name, path_to_sub_repo, url, branch='master'):
+    def _create_sub_module(self, sub_repo_name, path_to_sub_repo, url, branch='master'):
+        """
+        Creating a submodule
+        :param sub_repo_name: Name for the sub module
+        :param path_to_sub_repo: Path to the submodule
+        :param url: URL of the remote repo
+        :param branch:
+        :return:
+        """
         self.repo.create_submodule(sub_repo_name, path_to_sub_repo, url, branch)
 
-    @abstractmethod
-    def clone(self, url, path, branch='master'):
+    def _clone(self, url, path, branch='master'):
+        """
+        Clone a remote repo
+        :param url: URL of the remote remo
+        :param path: Path to clone the remote repo
+        :param branch: Branch to pull from the remote repo
+        :return: None
+        """
         if self.repo is not None:
             self.repo.clone_from(url, path, branch)
 
-    @abstractmethod
-    def _commit(self):
-        self.repo.index.commit()
+    def _commit(self, message):
+        self.repo.git.commit(message=message)
 
-    @abstractmethod
-    def get_untracked_files(self):
+    def _add_files(self, file_path):
+        """
+        Adds the untracked files to the git
+        :param file_path: An array containing the file paths to be added to git
+        :return:
+        """
+        if self._repo is None:
+            return
+
+        self._repo.index.add([file_path])
+
+    def _get_untracked_files(self):
         return self.repo.untracked_files
 
-    @abstractmethod
-    def get_tags(self):
+    def _get_tags(self):
         return self.repo.tags
 
-    @abstractmethod
-    def get_working_tree_directory(self):
+    def _get_working_tree_directory(self):
         return self.repo.working_tree_dir
 
-    @abstractmethod
-    def get_working_directory(self):
+    def _get_working_directory(self):
         return self.repo.working_dir
 
-    @abstractmethod
-    def get_git_path(self):
+    def _get_git_path(self):
         return self.repo.git_dir
 
-    @abstractmethod
-    def is_head_remote(self):
+    def _is_head_remote(self):
         return self.repo.head.is_remote()
 
-    @abstractmethod
-    def is_head_valid(self):
+    def _is_head_valid(self):
         return self.repo.head.is_valid()
 
-    @abstractmethod
-    def get_heads(self):
+    def _get_heads(self):
         return self.repo.heads
 
-    @abstractmethod
-    def check_git_directory(self, path):
+    def _check_git_directory(self, path):
         return self.repo.git_dir.startswith(path)
 
-    @abstractmethod
-    def get_head(self):
+    def _get_head(self):
         return self.repo.head
 
-    @abstractmethod
-    def delete_tags(self, tag_name):
+    def _delete_tags(self, tag_name):
         tags = self.repo.tags
         if tag_name in tags:
             self.repo.delete_tag(tag_name)
@@ -96,39 +138,73 @@ class IBaseGitBackend(IBaseMetaFileBackend):
             # Raise warning/error that tag is not present
             pass
 
-    @abstractmethod
-    def archive_repo(self, path):
+    def _archive_repo(self, path):
         with open(path, 'wb') as fp:
             self.repo.archive(fp)
 
-    @abstractmethod
-    def pull(self, name=None):
+    def _pull(self, name=None):
         origin = self.repo.remote(name=name)
         origin.pull()
 
-    @abstractmethod
-    def push(self, name=None):
-        origin = self.repo.remote(name=name)
-        origin.push()
+    def _push(self):
+        if self._remote is None:
+            self._remote = self._repo.create_remote(self._REMOTE_NAME, self._REMOTE_URL)
+
+        # Push to the master branch from current master branch
+        # https://gitpython.readthedocs.io/en/stable/reference.html#git.remote.Remote.push
+        self._remote.push(refspec='{}:{}'.format('master', 'master'))
 
     # database backend functions
-    def list_datasets(self, search_id=None, search_metadata=None):
+    def list(self, search_id=None, search_metadata=None):
         """
-        Function to list datasets. Will not be implemented for the Git Backend
+        Function to list repos. Cannot be implemented in GitPython.
+
         :param search_id:
         :param search_metadata:
         :return:
         """
-        pass
+        # TODO: Possibly, look for in the remote repositories if possible
+        super().list(search_id, search_metadata)
 
-    def put_dataset(self, dataset):
-        pass
+    def put(self, object):
+        """
+        Writes the object to the git backend. If necessary, will create a new repo
+        :param object: Object to be written to the repo
+        :return:
+        """
+        if self._repo is None:
+            self._create_repo(bare=False)
 
-    def get_dataset(self, dataset_id, metadata_only=False):
-        pass
+        # Write the files to the disk
+        super().put(object)
+        # Add the untracked file to the git
+        self._add_files()
+        # Call git commit
+        self._repo.commit(message=self._DEFAULT_GIT_MSG)
 
-    def delete_dataset(self, id):
-        pass
+    def get(self, path=None, url=None,  **kwargs):
+        """
+        Pull a repository from remote
+        :param repo_name: Name of the repo to be cloned
+        :param path: Path to be cloned
+        :return:
+        """
+        #repo_url = '/'.join(self._REMOTE_URL, repo_name)
+        if path is not None and url is not None:
+            self._repo = Repo.clone_from(url=url, to_path=path)
+
+        super().get(**kwargs)
+
+    def delete(self, id):
+        """
+        Deletes a repo from the Git backend
+        :param id: id of the repo to be deleted
+        :return:
+        """
+        # TODO: Remove the local directory
+        # TODO: User will have to remove the remote repository by themselves
+        super().delete(id)
+
 
 
 
--- pypadre/backend/remote/repository/github.py
+++ pypadre/backend/remote/repository/github.py
@@ -41,144 +41,34 @@ class GitHubBackend(i_base_git_backend):
         """
         self._repo.delete_file(path, commit_message, commit_sha, branch=branch)
 
-    def list_experiments(self, search_id=".*", search_metadata=None, start=-1, count=999999999,
-                         remote=True):
-
-        repo_list = []
-        # Add all the repositories of the user to an array
-        for repo in self._git.get_user(self._user.login).get_repos('all'):
-            repo_list.append(repo.name)
-
-        # TODO: Process the list
-
-        return repo_list
-
-    def delete_experiments(self, experiment=".*", mode="all", search_metadata=None):
+    def list(self, **kwargs):
         """
-        Padre does not support removal of experiments on the GitHub
-        :param experiment:
-        :param mode:
-        :param search_metadata:
+        List all the repositoreis
+        :param kwargs:
         :return:
         """
         pass
 
-    def put_experiment(self, experiment, append_runs=False, allow_overwrite=True):
-
-        repo_exists = False
-        # Check if repo already exists
-
-        if not repo_exists:
-            self.create_repo(experiment.name)
-
-        filename = "metadata.json"
-        comment = "Creating metadata.json at root experiment directory"
-        content = self._metadata_serializer.serialise(experiment.metadata)
-        branch = "master"
-        created_file = self.create_file(filename, comment, content, branch)
-
-
-    def get_experiment(self):
+    def put(self, **kwargs):
         """
-        Clones the experiment from the github repository
-        https://stackoverflow.com/questions/46937032/using-the-node-github-api-to-clone-a-remote-repo-locally
+        Write a file, create a new repository if needed
+        :param kwargs:
         :return:
         """
-        # Use the local git package to clone the repo locally
         pass
 
-    def put_experiment_configuration(self, experiment):
+    def get(self, id):
         """
-        Serializes the experiment configuration for purposes of sharing
-        :param experiment: The experiment object
+        Get the object specified
+        :param id:
         :return:
         """
-        # TODO Check if the experiment
-        if experiment.experiment_configuration is not None:
-            filename = "experiment.json"
-            comment = "Creating experiment configuration at root experiment directory"
-            content = self._metadata_serializer.serialise(experiment.metadata)
-            branch = "master"
-            created_file = self.create_file(filename, comment,
-                                            self._metadata_serializer.serialise(experiment.experiment_configuration),
-                                            branch)
+        pass
 
-    def put_run(self, experiment, run):
+    def delete(self, id):
         """
-        Stores a run of an experiment to the file repository.
-        :param experiment: experiment the run is part of
-        :param run: run to put
+        Delete the object specified by the id
+        :param id:
         :return:
         """
-        if run.id is None:  # this is a new experiment
-            run.id = uuid.uuid4()
-
-        filename = '/'.join([run.id, "metadata.json"])
-        comment = "Creating run directory"
-        content = self._metadata_serializer.serialise(run.metadata)
-        created_file = self.create_file(filename, comment,
-                                        content,
-                                        self._branch)
-
-        filename = os.path.join([run.id, "hyperparameter.json"])
-        comment = "Creating run directory"
-        content = self._metadata_serializer.serialise(run.metadata)
-        created_file = self.create_file(filename, comment,
-                                        self._metadata_serializer.serialise(experiment.hyperparameters()),
-                                        self._branch)
-
-    def put_split(self, experiment, run, split):
-        """
-        Stores a run of an experiment to the file repository.
-        :param experiment: experiment the run is part of
-        :param run: run to put
-        :return:
-        """
-        if split.id is None:  # this is a new experiment
-            split.id = uuid.uuid4()
-
-        filename = '/'.join([run.id, str(split.id), 'metadata.json'])
-        comment = "Creating split directory"
-        content = self._metadata_serializer.serialise(split.metadata)
-        created_file = self.create_file(filename, comment, content, self._branch)
-
-    def put_results(self, experiment, run, split, results):
-        """
-        Write the results of a split to the backend
-
-        :param experiment: Experiment ID
-        :param run_id: Run ID of the current experiment run
-        :param split_id: Split id
-        :param results: results to be written to the backend
-
-        :return: None
-        """
-        filename = '/'.join([run.id, str(split.id), 'results.json'])
-        comment = "Creating results file"
-        content = self._metadata_serializer.serialise(results)
-        created_file = self.create_file(filename, comment, content, self._branch)
-
-    def put_metrics(self, experiment, run, split, metrics):
-        """
-        Writes the metrics of a split to the backend
-
-        :param experiment: Experiment ID
-        :param run: Run Id of the experiment
-        :param split: Split ID
-        :param metrics: dictionary containing all the required metrics to be written to the backend
-
-        :return: None
-        """
-
-        filename = '/'.join([run.id, str(split.id), 'metrics.json'])
-        comment = "Creating metrics file"
-        content = self._metadata_serializer.serialise(metrics)
-        created_file = self.create_file(filename, comment, content, self._branch)
-
-    def log_end_experiment(self):
-
-        filename = "log.txt"
-        comment = "Ccompleting the experiment. Uploading the log file"
-        content = ""
-
-        created_file = self.create_file(filename, comment, content, self._branch)
+        pass
