dd44237be0fba31d02f8ad8889fb80b9fdcba219
christofer <fellic01@gw.uni-passau.de>
2019-10-01 15:52:42 +0200

    Added setupClass and teardownClass methods for initialization of tests

--- pypadre/core/tests/padre_test.py
+++ pypadre/core/tests/padre_test.py
@@ -7,28 +7,34 @@ from pypadre.pod.app import PadreConfig
 
 
 class PadreTest(unittest.TestCase):
-
-    def setUp(self):
-        self.config_path = os.path.join(os.path.expanduser("~"), ".padre-test.cfg")
-        self.workspace_path = os.path.join(os.path.expanduser("~"), ".pypadre-test")
+    @classmethod
+    def setUpClass(cls):
+        cls.config_path = os.path.join(os.path.expanduser("~"), ".padre-test.cfg")
+        cls.workspace_path = os.path.join(os.path.expanduser("~"), ".pypadre-test")
 
         """Create config file for testing purpose"""
-        self.config = configparser.ConfigParser()
+        cls.config = configparser.ConfigParser()
         test_data = {'test_key': 'value 1', 'key2': 'value 2'}
-        self.config['TEST'] = test_data
-        with open(self.config_path, 'w+') as configfile:
-            self.config.write(configfile)
+        cls.config['TEST'] = test_data
+        with open(cls.config_path, 'w+') as configfile:
+            cls.config.write(configfile)
 
-        self.config = PadreConfig(config_file=self.config_path)
-        self.config.set("backends", str([
+        cls.config = PadreConfig(config_file=cls.config_path)
+        cls.config.set("backends", str([
             {
-                "root_dir": self.workspace_path
+                "root_dir": cls.workspace_path
             }
         ]))
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         """Remove config file after test"""
-        os.remove(self.config_path)
+        os.remove(cls.config_path)
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
         try:
 
             if os.path.exists(os.path.join(self.workspace_path,"datasets")):
@@ -39,3 +45,7 @@ class PadreTest(unittest.TestCase):
 
         except FileNotFoundError:
             pass
+
+        print('Teardown of test')
+
+
