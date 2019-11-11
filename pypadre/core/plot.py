c906a952c3fe759871cad98548638d0963ddf4de
afnan <hm.afnan@yahoo.com>
2019-11-11 12:43:20 +0100

    Charts added in plot.py

--- pypadre/core/plot.py
+++ pypadre/core/plot.py
@@ -1,9 +1,33 @@
+"""
+Plots are implemented with Altair which is Python implementation of Vega-Lite
+"""
+import json
+import os
+
 import altair as alt
+import numpy as np
+import pandas as pd
+
+from sklearn import metrics
 
 
 class Plot:
     """
     Class providing commands for managing visualizations.
+    """
+    def get_chart_from_json(self, visualisation):
+        """
+        Get altair Chart from json to render in notebook
+        :param visualisation: Json specification of vega lite
+        :returns: Altair Chart
+        :rtype: <class 'altair.vegalite.v2.api.Chart'>
+        """
+        return alt.Chart.from_json(visualisation)
+
+
+class DataPlot(Plot):
+    """
+    Class providing commands for creating visualizations for Data sets.
     """
     def __init__(self, dataset):
         self._dataset = dataset
@@ -36,12 +60,199 @@ class Plot:
             title="%s dataset visualization for %s and %s" % (self._dataset.name, x_attr, y_attr)).interactive()
         return chart.to_json()
 
-    def get_chart_from_json(self, visualisation):
+    def plot_correlation_matrix(self, title="Feature Correlation Matrix"):
+        """Plot pearson correlation matrix between features.
+
+        if number of features are less than 30 then show text inside matrix otherwise just show colors
+        Supports numpy and pandas formats
+
+        :param title: Title of the chart
+        :return: Returns vega lite specification of the chart.
         """
-        Get altair Chart from json to render in notebook
-        :param visualisation: Json specification of vega lite
-        :returns: Altair Chart
-        :rtype: <class 'altair.vegalite.v2.api.Chart'>
+        if self._dataset.binary_format() == 'numpy':
+            mx = pd.DataFrame(self._dataset.data).corr('pearson')
+        else:
+            mx = self._dataset.data.corr('pearson')
+        total_features = mx.shape[1]
+        x, y = np.meshgrid(range(0, total_features), range(0, total_features))
+        source = pd.DataFrame(
+            {'x': x.ravel(), 'y': y.ravel(), 'z': np.around(np.array(mx).ravel(), decimals=1)})
+        # Adjust width and height of the chart
+        if total_features <= 15:
+            width, height = 500, 450
+        else:
+            width, height = 650, 550
+
+        base = alt.Chart(source).encode(
+            alt.X('x:O',
+                  title='Features'
+                  ),
+            alt.Y('y:O', title='Features')
+        ).properties(width=width, height=height)
+
+        chart = base.mark_rect().encode(
+            alt.Color('z:Q', title='Range', scale=alt.Scale(scheme='redblue'))
+        ).properties(title=title)
+        if total_features <= 30:
+            text = base.mark_text(baseline='middle').encode(
+                text='z:Q',
+                color=alt.condition(
+                    'datum.z > 0.5 || datum.z < 0.0',
+                    alt.value('white'),
+                    alt.value('black')
+                )
+            )
+            chart = chart + text
+        return chart.to_json()
+
+    def plot_class_balance(self, title='Class Balance'):
+        """Plot class balance chart.
+
+        Treats classification data as nominal type and regression data as quantitative type.
+        Supports numpy and pandas formats
+
+        :param title: Title of the chart
+        :return: Returns vega lite specification of the chart.
         """
-        return alt.Chart.from_json(visualisation)
+        target = None
+        if type(self._dataset.data) == np.ndarray:
+            df = pd.DataFrame()
+            for index, attr in enumerate(self._dataset.attributes):
+
+                df[attr['name']] = self._dataset.data[:, index]
+                if attr['defaultTargetAttribute']:
+                    target = attr['name']
+        else:
+            df = self._dataset.data
+            columns = []
+            for attr in self._dataset.attributes:
+                columns.append(attr['name'])
+                if attr['defaultTargetAttribute']:
+                    target = attr['name']
+            df.columns = columns
+        if df[target].nunique() <= 15:  # Treat as classification data
+            chart = alt.Chart(df).mark_bar().encode(
+                x=alt.X('count()', title='Total Records'),
+                y=alt.Y(target+':N', title='Class Labels'),
+                color=alt.Color(target + ':N', title='Labels')).properties(title=title)
+        else:  # Treat as regression data
+            chart = alt.Chart(df).mark_bar().encode(
+                x=alt.X('count()', title='Range Count'),
+                y=alt.Y(target + ':Q', title='Data Points'),
+                color=alt.Color(target + ':Q', title='Range')).properties(title=title)
+        return chart.to_json()
+
+
+class ExperimentPlot(Plot):
+    """
+    Class providing commands for creating visualizations for experimental results.
+    """
+    def __init__(self, experiment_id, run_id, split_id):
+        self._experiment_id = experiment_id + '.ex'
+        self._run_id = run_id + '.run'
+        self._split_id = split_id + '.split'
+        self._experiment_path = os.path.join(os.path.expanduser("~/.pypadre"), *["experiments", self._experiment_id])
+
+    def split_path(self):
+        split_path = os.path.join(self._experiment_path, *[self._run_id, self._split_id])
+        if os.path.exists(split_path):
+            return split_path
+        else:
+            raise NotADirectoryError("Split path does not exists")
+
+    def plot_confusion_matrix(self, title='Confusion Matrix'):
+        """Plot confusion matrix and return its json specification of vega-lite
+
+        :param title: Title for the chart
+        :returns: Json specification of Vega-Lite
+        """
+        split_path = self.split_path()
+        with open(os.path.join(split_path, "metrics.json")) as f:
+            metrics = json.loads(f.read())
+        cm = np.array(metrics["confusion_matrix"])
+        max_value = cm.max()
+        x, y = np.meshgrid(range(0, cm.shape[0]), range(0, cm.shape[0]))
+        source = pd.DataFrame({'x': x.ravel(), 'y': y.ravel(), 'z': cm.ravel()})
+        base = alt.Chart(source).encode(
+            alt.X('x:O', title='Predicted label'),
+            alt.Y('y:O', title='True label')
+        ).properties(width=400, height=400)
+
+        heatmap = base.mark_rect().encode(
+            alt.Color('z:Q', title='Value Range', scale=alt.Scale(scheme='purplebluegreen'))
+        ).properties(title=title)
+
+        text = base.mark_text(baseline='middle').encode(
+            text='z:Q',
+            color=alt.condition(
+                alt.datum.z < max_value / 3,
+                alt.value('black'),
+                alt.value('white')
+            )
+        )
+
+        chart = heatmap + text
+        return chart.to_json()
+
+    def plot_pr_curve(self, title='Precision Recall Curve'):
+        """Plot precision recall curve and return its json specification of vega-lite
+
+        :param title: Title for the chart
+        :returns: Json specification of Vega-Lite
+        Todo: Currently only implemented for binary classification, later add functionality for multi class classification
+        """
+        split_path = self.split_path()
+        with open(os.path.join(split_path, "results.json"), 'r') as f:
+            results = json.loads(f.read())
+        probabilities = results["probabilities"]
+        truths = results["truth"]
+        if len(probabilities[0]) > 2:
+            raise NotImplementedError("PR Plot only implemented for binary classification")
+        score = [x[-1] for x in probabilities]
+        precision, recall, thresholds = metrics.precision_recall_curve(truths, score)
+        data = pd.DataFrame({
+            "x": recall,
+            "y": precision
+        })
+        chart = alt.Chart(data).mark_area(
+            color="lightblue",
+            interpolate='step-after',
+            line=True
+        ).encode(
+            x=alt.X('x', title="Recall"),
+            y=alt.Y('y', title="Precision")
+        ).properties(title=title)
+        return chart.to_json()
+
+    def plot_roc_curve(self, title='ROC Curve'):
+        """Plot ROC curve and return its json specification of vega-lite
+
+        :param title: Title for the chart
+        :returns: Json specification of Vega-Lite
+        Todo: Currently only implemented for binary classification, later add functionality for multi class classification
+        """
+        split_path = self.split_path()
+        with open(os.path.join(split_path, "results.json"), 'r') as f:
+            results = json.loads(f.read())
+        probabilities = results["probabilities"]
+        truths = results["truth"]
+        if len(probabilities[0]) > 2:
+            raise NotImplementedError("ROC Plot only implemented for binary classification")
+        score = [x[-1] for x in probabilities]
+        fpr, tpr, thresholds = metrics.roc_curve(truths, score)
+        data = pd.DataFrame({
+            "x": fpr,
+            "y": tpr
+        })
+        chart = alt.Chart(data).mark_area(
+            color="lightblue",
+            interpolate='step-after',
+            line=True
+        ).encode(
+            x=alt.X('x', title="False Positive Rate"),
+            y=alt.Y('y', title="True Positive Rate")
+        ).properties(title=title)
+        return chart.to_json()
+
+
 
