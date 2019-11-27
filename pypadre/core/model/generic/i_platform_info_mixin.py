99e927cad7d0db400ecc5ac10a1e26d0a60fabf7
Thomas Weißgerber <weissger@posteo.de>
2019-11-27 13:38:52 +0100

    platform infos, requirements

---
+++ pypadre/core/model/generic/i_platform_info_mixin.py
@@ -0,0 +1,32 @@
+import platform
+from abc import ABCMeta, abstractmethod
+
+from pypadre.core.base import MetadataMixin
+
+
+class PlatformInfoMixin(MetadataMixin):
+    """ This is the abstract class for all entities holding platform metadata."""
+    __metaclass__ = ABCMeta
+
+    def _tablefy_register_columns(cls):
+        super()._tablefy_register_columns()
+        cls.tablefy_register("platform_info")
+
+    @abstractmethod
+    def __init__(self, *args, metadata=None, **kwargs):
+
+        sys_info = dict()
+        sys_info["processor"] = platform.processor()
+        sys_info["machine"] = platform.machine()
+        sys_info["system"] = platform.system()
+        sys_info["platform"] = platform.platform()
+        sys_info["platform_version"] = platform.version()
+        sys_info["node_name"] = platform.node()
+        sys_info["python_version"] = platform.python_version()
+        metadata["platform_info"] = sys_info
+
+        super().__init__(metadata=metadata, **kwargs)
+
+    @property
+    def platform_info(self):
+        return self.metadata["platform_info"]
