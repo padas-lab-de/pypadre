0501e2ac37f2a3ec1c283326db78c361d1cded43
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-17 20:16:00 +0000

    Adding tarfile member sanitization to extractall()

--- pypadre/pod/importing/dataset/graph_import.py
+++ pypadre/pod/importing/dataset/graph_import.py
@@ -404,7 +404,26 @@ def create_from_konect(url, zero_based=False):
         ftemp.seek(0)
         with tempfile.TemporaryDirectory(suffix=name) as tempdir:
             with tarfile.open(fileobj=ftemp, mode='r:bz2') as tar:
-                tar.extractall(path=tempdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=tempdir)
 
                 for root, dirs, files in os.walk(tempdir):
                     for file in files:
